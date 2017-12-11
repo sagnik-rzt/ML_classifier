31b7173875fc9c34728bbff80e93925e102ac3a6
sagnik-rzt <sagnik.bhattacharya@razorthink.com>
2017-12-11 18:14:44 +0530

    Added variable.assign() operation

--- tf_basics.py
+++ tf_basics.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 a = tf.Variable(initial_value = [5,6,7,8,9,10], name="var1")
 b = tf.constant(6)
 c = tf.placeholder(dtype = tf.int32)
+d = a.assign(a + tf.constant([1,1,1,1,1,1]))
 
 x = a * b + c
 tf.add_to_collection(name="var1", value=x)
@@ -15,10 +16,15 @@ init = tf.global_variables_initializer()
 with tf.Session() as sess :
     sess.run(init)
     print(sess)
-    result = sess.run(x, feed_dict = {c : [0,1,2,3,4,5]})
-    print(result)
+    result1 = sess.run(x, feed_dict = {c : [0,1,2,3,4,5]})
+    print(result1)
     print('\n')
-    print(result.shape)
+    print(result1.shape)
+
+    result2 = sess.run(d)
+    print(result2)
+    result3 = sess.run(d)
+    print(result3)
     writer = tf.summary.FileWriter("output", sess.graph)
 
     saver.save(sess, save_path="saver/test.model")
