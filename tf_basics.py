f1281c5b1807e6c468ecfbf358443f6f66a8c50b
sagnik-rzt <sagnik.bhattacharya@razorthink.com>
2017-12-11 18:21:20 +0530

    Added a clock-counter variable

--- tf_basics.py
+++ tf_basics.py
@@ -4,6 +4,7 @@ a = tf.Variable(initial_value = [5,6,7,8,9,10], name="var1")
 b = tf.constant(6)
 c = tf.placeholder(dtype = tf.int32)
 d = a.assign(a + tf.constant([1,1,1,1,1,1]))
+counter = tf.Variable(initial_value = 0)
 
 x = a * b + c
 tf.add_to_collection(name="var1", value=x)
@@ -18,13 +19,14 @@ with tf.Session() as sess :
     print(sess)
     result1 = sess.run(x, feed_dict = {c : [0,1,2,3,4,5]})
     print(result1)
+    print("Counter is", sess.run(counter.assign_add(1)))
     print('\n')
-    print(result1.shape)
 
     result2 = sess.run(d)
     print(result2)
     result3 = sess.run(d)
     print(result3)
     writer = tf.summary.FileWriter("output", sess.graph)
+    print("Counter is", sess.run(counter.assign_add(1)))
 
     saver.save(sess, save_path="saver/test.model")
